b8297393f589998af34612fc3cfb4137f13fa81b
Mila-MP <mila.pertuymorris@gmail.com>
2025-11-02 13:42:46 +0100

    minor improvements

--- train_llm/llm_training.py
+++ train_llm/llm_training.py
@@ -27,17 +27,17 @@ if torch.cuda.is_available():
 
 ### Define model to use and output directory
 
-# model_name = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
-# model_output_dir = "PMB_model"
-# checkpoint_dir = "PMB_model_checkpoints"
+model_name = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext"
+model_output_dir = "models/fine_tuned_BERT_models/PMB_model"
+checkpoint_dir = "models/fine_tuned_BERT_models/PMB_model_checkpoints"
 
 # model_name = "google-bert/bert-base-uncased"
-# model_output_dir = "BERT_model"
-# checkpoint_dir = "BERT_model_checkpoints"
+# model_output_dir = "models/fine_tuned_BERT_models/BERT_model"
+# checkpoint_dir = "models/fine_tuned_BERT_models/BERT_model_checkpoints"
 
-model_name = "google-bert/bert-base-uncased"
-model_output_dir = "test_model"
-checkpoint_dir = "test_model_checkpoints"
+# model_name = "google-bert/bert-base-uncased"
+# model_output_dir = "models/fine_tuned_BERT_models/test_model"
+# checkpoint_dir = "models/fine_tuned_BERT_models/checkpoints_test_model"
 
 
 print(f"Model chosen: {model_name}")
@@ -126,42 +126,42 @@ def save(trainer, tokenizer, save_dir):
     print(f"Model and Tokenizer saved to {save_dir}")
 
 
-# args = TrainingArguments(
-#     output_dir=checkpoint_dir,
-#     eval_strategy="epoch",
-#     save_strategy="epoch",
-#     save_total_limit=1,
-#     load_best_model_at_end=True,
-#     metric_for_best_model="macro_f1",
-#     greater_is_better=True,
-#     per_device_train_batch_size=16,
-#     per_device_eval_batch_size=32,
-#     learning_rate=2e-5,
-#     num_train_epochs=1,
-#     weight_decay=0.01,
-#     warmup_ratio=0.1,
-#     logging_steps=50,
-#     seed=42,
-#     remove_unused_columns=True,
-# )
-
-# Use these arguments to have a very quick training and just check that the script runs fine
-
 args = TrainingArguments(
     output_dir=checkpoint_dir,
-    eval_strategy="no",  # skip evaluation for speed
-    save_strategy="no",  # don't save checkpoints
-    per_device_train_batch_size=2,  # smaller batch size
-    per_device_eval_batch_size=2,
-    learning_rate=5e-5,  # slightly higher LR for faster convergence
-    num_train_epochs=0.05,  # fraction of an epoch (if supported) or 1 if not
-    max_steps=20,  # stop after 20 steps regardless of dataset size
-    weight_decay=0.0,
-    warmup_ratio=0.0,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    save_total_limit=1,
+    load_best_model_at_end=True,
+    metric_for_best_model="macro_f1",
+    greater_is_better=True,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=32,
+    learning_rate=2e-5,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    warmup_ratio=0.1,
     logging_steps=50,
     seed=42,
     remove_unused_columns=True,
 )
+
+# Use these arguments to have a very quick training and just check that the script runs fine
+
+# args = TrainingArguments(
+#     output_dir=checkpoint_dir,
+#     eval_strategy="no",  # skip evaluation for speed
+#     save_strategy="no",  # don't save checkpoints
+#     per_device_train_batch_size=2,  # smaller batch size
+#     per_device_eval_batch_size=2,
+#     learning_rate=5e-5,  # slightly higher LR for faster convergence
+#     num_train_epochs=0.05,  # fraction of an epoch (if supported) or 1 if not
+#     max_steps=20,  # stop after 20 steps regardless of dataset size
+#     weight_decay=0.0,
+#     warmup_ratio=0.0,
+#     logging_steps=50,
+#     seed=42,
+#     remove_unused_columns=True,
+# )
 
 trainer = WeightedTrainer(
     model=base_model,
